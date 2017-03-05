0fd29a83398ec79dfb5fc36319696d89c1723708
pi <pi@pi.pi>
2017-03-05 17:30:58 +0800

    it can control stm32 now

--- SimCom.c
+++ SimCom.c
@@ -4,9 +4,16 @@
 #ifdef TEST_SERVICE
 
 #include <stdio.h>
+#include <math.h>
 #include <unistd.h>
 #include "ServiceLayer.h"
 #include "PhysicalLayer.h"
+
+#define M_ERR 0.1f
+
+float x_now = 0;
+float y_now = 0;
+float angle_now = 0;
 
 void callback0(char from, char to, char* data, SIMCOM_LENGTH_TYPE length)
 {
@@ -18,14 +25,31 @@ void callback0(char from, char to, char* data, SIMCOM_LENGTH_TYPE length)
     }
     pf = (float*)ldata;
     printf("x=%f\ty=%f\tangle=%f\n", pf[0], pf[1], pf[2]);
+    x_now = pf[0];
+    y_now = pf[1];
+    angle_now = pf[2];
   } else {
     printf("length=%d\n", length);
   }
 }
 
-void callback2(char from, char to, char* data, SIMCOM_LENGTH_TYPE length)
+void callback1(char from, char to, char* data, SIMCOM_LENGTH_TYPE length)
 {
-  printf("Welcome to callback2\n");
+  printf("Callback 1:");
+  for(SIMCOM_LENGTH_TYPE i = 0; i < length; i++) {
+    putchar(data[i]);
+  }
+}
+
+void move_to_point(float x, float y, float angle) {
+  float tmp[3];
+  tmp[0] = x;
+  tmp[1] = y;
+  tmp[2] = angle;
+
+  char *pi;
+  pi = (char*)tmp;
+  sl_send(0, 0, pi, 12);
 }
 
 int main()
@@ -35,24 +59,39 @@ int main()
   SIMCOM_LENGTH_TYPE length;
 
   sl_config(0, callback0);
+  sl_config(1, callback1);
 
   if(!sl_init()) {
     printf("Unable to open the serial port\n");
     return -1;
   }
 
-  sl_send(0, 2, "", 0);
-  sl_send(0, 3, "", 0);
-  sl_send(0, 0, "Hello, World!Hello, World!\n", 55);
-  sl_send(0, 1, "Hello, World!Hello, World!\n", 55);
-  sl_send(0, 0, "Hello, World!Hello, World!\n", 55);
-  sl_send(0, 1, "Hello, World!Hello, World!\n", 55);
-  sl_send(1, 2, "", 0);
+
+  int count = 0;
+  int step = 0;
+
 
   while(1) {
     ph_send_intr();
     sl_receive_intr();
     usleep(100);
+    if(count < 4000) {
+      count++;
+    } else {
+      float x[] = {0, 3, 0, 0, 3, 0, 0, 3, 0, 0, 3, 0, 0, 3, 0};
+      float y[] = {2.5, 0, 0, 2.5, 0, 0, 2.5, 0, 0, 2.5, 0, 0, 2.5, 0, 0};
+      float angle[] = {0, -2.266, 0, 0, -2.266, 0, 0, -2.266, 0, 0, -2.266, 0, 0, -2.266, 0};
+      move_to_point(x[step], y[step], angle[step]);
+      printf("moving to %f, %f, %f\n", x[step], y[step], angle[step]);
+      if(step < 14\
+        && fabsf(x[step] - x_now) < M_ERR\
+        && fabsf(y[step] - y_now) < M_ERR\
+        && fabsf(angle[step] - angle_now) < M_ERR) {
+        step++;
+        usleep(500000);
+      }
+      count = 0;
+    }
   }
 
   return 0;
